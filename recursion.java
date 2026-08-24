1255d01ba26677b7e44129d0a4e472f9c0fa87de
Sutej V <mteja9262@gmail.com>
2026-08-24 15:22:35 +0530

    Simple recursion

---
+++ recursion.java
@@ -0,0 +1,17 @@
+public class recursion {
+
+    static void print(int n){
+
+        if(n==6)      
+            return;
+
+        System.out.println(n);
+
+        print(n+1);    
+    }
+
+    public static void main(String[] args){
+
+        print(1);
+    }
+} 
