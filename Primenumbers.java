13eba7ba5006f734c93c7a606dee87fd4d417a0b
Sutej V <mteja9262@gmail.com>
2026-08-24 15:18:34 +0530

    Prime Check

---
+++ Primenumbers.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Primenumbers {
+
+    public static boolean isPrime(int n){
+
+        if (n<=1) {
+            return false;
+        }
+
+        if (n==2){
+            return true;
+        }
+
+        if (n % 2 == 0) {
+            return false;
+        }
+
+        for(var i = 3;i<=Math.sqrt(n);i += 2){
+            if(n%i==0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+    
+    public static void main(String[] args){
+        Scanner Prime = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        var n = Prime.nextInt();
+
+        if(isPrime(n)){
+            System.out.println(n+" is a prime number.");
+        }else{
+            System.out.println(n+" is not a prime number.");
+        }
+
+        Prime.close();
+        
+        
+    }
+}
